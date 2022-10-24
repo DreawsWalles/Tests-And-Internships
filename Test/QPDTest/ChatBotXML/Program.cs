1748279c5f2ffd155681251892260563c41b09f8
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2022-10-24 17:33:54 +0300

    Чат-бот с использование xml файлов

---
+++ Test/QPDTest/ChatBotXML/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpClasses;
+
+namespace ChatBotXML
+{
+    public class Program
+    {
+        static public void Main()
+        {
+            ChatBotXML bot = new ChatBotXML();
+            Console.WriteLine(bot.Ask("Привет"));
+            string question;
+            do
+            {
+                Console.Write("->");
+                question = Console.ReadLine();
+                Console.WriteLine(question = bot.Ask(question));
+            } while (question != "Пока" && question != "До свидания");
+            HelpFunctions.Continue();
+        }
+    }
+}
