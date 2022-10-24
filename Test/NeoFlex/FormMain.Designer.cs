fa231f210c3a570a324cdb25e209a57e3dc312d0
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2022-10-24 18:08:09 +0300

    Тестовое задание
    Задача: Реализовать winForm приложение, которые считавает текст с файла, выводит его на форму. По нажатию на кнопку запуск, запускается второй потом и вставляет введенный текст в считанный текст. При закрытии формы должно происходить остановка процесса

---
+++ Test/NeoFlex/FormMain.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace project
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textFile = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.5F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(798, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.btnLoad, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnStartStop, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(501, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(294, 444);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textFile);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(492, 444);
+            this.panel1.TabIndex = 1;
+            // 
+            // textFile
+            // 
+            this.textFile.BackColor = System.Drawing.Color.White;
+            this.textFile.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textFile.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textFile.Location = new System.Drawing.Point(34, 25);
+            this.textFile.Multiline = true;
+            this.textFile.Name = "textFile";
+            this.textFile.ReadOnly = true;
+            this.textFile.Size = new System.Drawing.Size(413, 391);
+            this.textFile.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnLoad.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLoad.Location = new System.Drawing.Point(67, 117);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(160, 27);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Загрузить файл";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnStartStop.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStartStop.Location = new System.Drawing.Point(67, 297);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(160, 27);
+            this.btnStartStop.TabIndex = 2;
+            this.btnStartStop.Text = "Запустить";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.textBox, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 150);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(288, 141);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.Location = new System.Drawing.Point(3, 38);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(282, 25);
+            this.textBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Текст для вставки:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Resize += new System.EventHandler(this.FormMain_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textFile;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Test/NeoFlex/FormMain.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class FormMain : Form
+    {
+        private bool _isWork = false;
+        private Random rnd = new Random();
+        public FormMain()
+        {
+            InitializeComponent();
+            textFile.Enabled = false;
+            btnStartStop.Enabled = false;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Size = new Size(Size.Width - 24, Size.Height - 43);
+            tableLayoutPanel2.Size = new Size(tableLayoutPanel1.Width, tableLayoutPanel1.Height);
+            panel1.Size = new Size(tableLayoutPanel1.Width, tableLayoutPanel1.Height);
+            textFile.Size = new Size(panel1.Width - 70, panel1.Height - 50);
+            tableLayoutPanel3.Size = new Size(tableLayoutPanel2.Width, tableLayoutPanel2.Height);
+        }
+
+        private void FormMain_Resize(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Size = new Size(Size.Width - 24, Size.Height - 43);
+            tableLayoutPanel2.Size = new Size(tableLayoutPanel1.Width, tableLayoutPanel1.Height);
+            panel1.Size = new Size(tableLayoutPanel1.Width, tableLayoutPanel1.Height);
+            textFile.Size = new Size(panel1.Width - 70, panel1.Height - 50);
+            tableLayoutPanel3.Size = new Size(tableLayoutPanel2.Width, tableLayoutPanel2.Height);
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(textBox.Text))
+            {
+                MessageBox.Show("Введите текст, необходимый для вставки", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if(!_isWork)
+            {
+                _isWork = true;
+                Thread thread = new Thread(new ThreadStart(MainTask));
+                thread.Start();
+            }
+            else
+            {
+                _isWork = false;
+            }
+        }
+        private void MainTask()
+        {
+            Invoke(new Action(() =>
+            {
+                textBox.Enabled = false;
+                btnStartStop.Text = "Остановить";
+                btnLoad.Enabled = false;
+            }));
+            while (_isWork)
+            {
+                Thread.Sleep(5000);
+                textBox.Invoke((MethodInvoker)delegate
+                {
+                    textFile.Text = textFile.Text.Insert(rnd.Next(0, textFile.Text.Length - 1), textBox.Text);
+                });                
+            }
+            Invoke(new Action(() =>
+            {
+                btnStartStop.Text = "Запустить";
+                btnLoad.Enabled = true;
+                textBox.Enabled = true;
+            }));
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog fileDialog = new OpenFileDialog())
+            {
+                fileDialog.Filter = "Text Files(*.TXT)|*.TXT";
+                if(fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (StreamReader file = new StreamReader(fileDialog.FileName))
+                        {
+                            textFile.Text = file.ReadToEnd();
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("При считывании информации из файла произошла ошибка", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    textFile.Enabled = true;
+                    btnStartStop.Enabled = true;
+                }
+            }
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _isWork = false;
+        }
+    }
+}
