5d36591b3e1708b28263f0d82cfe0e72d4a662e7
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2022-10-24 17:51:01 +0300

    Проект, объединяющий задачи по теме
    Тема:Реализация калькулятора и чат-бота

---
+++ Test/QPDTest/ThemeFour/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+using HelpClasses;
+
+namespace ThemeFour
+{
+    public class Program
+    {
+        static void TasksAboutThisTheme()
+        {
+
+        }
+        public static void Main()
+        {
+            Menu menu = new Menu("Выберите блок заданий");
+            menu.Add("Работа калькулятора");
+            menu.Add("Работа чат- бота");
+            menu.Add("Вопросы по данной теме");
+            menu.Add("Выйти в главное меню", true);
+            int choice;
+            do
+            {
+                menu.Print();
+                choice = menu.Choice();
+                Console.Clear();
+                switch(choice)
+                {
+                    case 1:
+                        Calculator.Program.Main();
+                        break;
+                    case 2:
+                        ChatBot.Program.Main();
+                        break;
+                    case 3:
+                        TasksAboutThisTheme();
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
