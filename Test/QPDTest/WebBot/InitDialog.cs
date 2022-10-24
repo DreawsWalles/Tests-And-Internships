8b81408c729b7bb713894fe474cca842ca75f732
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2022-10-24 17:43:15 +0300

    Реализация чат-бота с использованием asp.net core

---
+++ Test/QPDTest/WebBot/InitDialog.cs
@@ -0,0 +1,22 @@
+﻿using WebBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBot
+{
+    public static class InitDialog
+    {
+        public static void Initialize(Chat context)
+        {
+            if (context.dialogs.Any())
+            {
+                foreach (var el in context.dialogs)
+                    context.dialogs.Remove(el);
+            }
+            context.dialogs.AddRange(new Message { _Message = Bot.Ask("Привет") });
+            context.SaveChanges();
+        }
+    }
+}
