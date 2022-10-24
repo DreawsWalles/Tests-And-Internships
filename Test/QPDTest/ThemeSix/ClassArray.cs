70a1442140148a5ff0980ce7f32e5e9889a700ca
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2022-10-24 17:56:20 +0300

    Проект объединяющий задачи по теме
    Тема: работа с потоками

---
+++ Test/QPDTest/ThemeSix/ClassArray.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace ThemeSix
+{
+    static class ClassArray
+    {
+        static public int[] Array { get; private set; }
+        static int Size;
+        static ClassArray()
+        {
+            Size = 1000000;
+            Array = new int[Size];   
+        }
+        static public void InitOneThread()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < Size; i++)
+                Array[i] = rnd.Next(0, 900);
+        }
+        static public void InitMultyThread()
+        {
+            Parallel.Invoke(
+                () =>
+                {
+                    Random rand1 = new Random();
+                    for (int i = 0; i < Size / 2; i++)
+                        Array[i] = rand1.Next(0, 900);
+                },
+                () =>
+                {
+                    Random rand2 = new Random();
+                    for (int i = Size / 2; i < Size; i++)
+                        Array[i] = rand2.Next(0, 900);
+                });
+        }
+        static void Swap(ref int x, ref int y)
+        {
+            int t = x;
+            x = y;
+            y = t;
+        }
+
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+            for (int i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+        static void QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+                return;
+            int pivotIndex = Partition(array, minIndex, maxIndex);
+            Parallel.Invoke(
+                        () => QuickSort(array, minIndex, pivotIndex - 1),
+                        () => QuickSort(array, pivotIndex + 1, maxIndex));
+
+            return;
+        }
+        static public void QuickSort()
+        {
+            QuickSort(Array, 0, Array.Length - 1);
+        }
+        static void BubbleSort(int[] array, int indexBeg, int indexEnd, bool swapFlag)
+        {
+            if (indexBeg == indexEnd || !swapFlag)
+                return;
+            Parallel.Invoke(
+                () =>
+                {
+                    swapFlag = false;
+                    for (int i = indexBeg; i < indexEnd - 1; i++)
+                        if (array[i] > array[i + 1])
+                        {
+                            Swap(ref array[i], ref array[i + 1]);
+                            swapFlag = true;
+                        }
+                    BubbleSort(array, indexBeg + 1, indexEnd - 1, swapFlag);
+                },
+                () =>
+                {
+                    swapFlag = false;
+                    for (int i = indexEnd - 2; i > indexBeg; i--)
+                        if (array[i - 1] > array[i])
+                        {
+                            Swap(ref array[i - 1], ref array[i]);
+                            swapFlag = true;
+                        }
+                    BubbleSort(array, indexBeg + 1, indexEnd - 1, swapFlag);
+                });
+        }
+        static public void BubbleSort()
+        {
+            BubbleSort(Array, 0, Size, true);
+        }
+    }
+}
---
+++ Test/QPDTest/ThemeSix/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using HelpClasses;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace ThemeSix
+{
+    public class Program
+    {         
+        static void WorkWithArray()
+        {
+            Console.Clear();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            ClassArray.InitOneThread();
+            watch.Stop();
+            TimeSpan ts = watch.Elapsed;
+            Console.WriteLine("Время инициализации массива одним потоком: " + String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10));
+            watch.Reset();
+            watch.Start();
+            ClassArray.InitMultyThread();
+            watch.Stop();
+            ts = watch.Elapsed;
+            Console.WriteLine("Время инициализации массива несколькими потоками: " + String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10));
+            watch.Reset();
+            watch.Start();
+            ClassArray.QuickSort();
+            watch.Stop();
+            ts = watch.Elapsed;
+            Console.WriteLine("Время быстрой сортировки: " + String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10));
+            watch.Reset();
+            ClassArray.InitMultyThread();
+            watch.Start();
+            ClassArray.BubbleSort();
+            watch.Stop();
+            ts = watch.Elapsed;
+            Console.WriteLine("Время сортировки пузырьком: " + String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10));
+            HelpFunctions.Continue();
+            Console.Clear();
+        }
+        public static void Main()
+        {
+            Menu menu = new Menu("Выберите блок заданий:");
+            menu.Add("Работа с массивом");
+            menu.Add("Запустить \"Чат-бота\"");
+            menu.Add("Выйти в главное меню", true);
+            int choice;
+            do
+            {
+                menu.Print();
+                choice = menu.Choice();
+                switch (choice)
+                {
+                    case 1:
+                        WorkWithArray();
+                        break;
+                    case 2:
+                        ChatBotMultyThread.Program.Main();
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
