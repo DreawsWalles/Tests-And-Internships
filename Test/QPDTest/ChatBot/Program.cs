c3fc2ad5493c224f01f8b68ac6657fcc9f0afddb
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2022-10-24 17:32:49 +0300

    Реализация консольного чат-бота

---
+++ Test/QPDTest/ChatBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpClasses;
+
+namespace ChatBot
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(ChatBot.Ask("Привет"));
+            string question;
+            do
+            {
+                Console.Write("->");
+                question = Console.ReadLine();
+                Console.WriteLine(question = ChatBot.Ask(question));
+            } while (question != "Пока" && question != "До свидания");
+            HelpFunctions.Continue();
+        }
+    }
+}
