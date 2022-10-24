a1b90c7e38f423a3d1aafdf0862d5e6001b2083d
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2022-10-24 17:33:27 +0300

    Многопоточный чат-бот

---
+++ Test/QPDTest/ChatBotMultyThread/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HelpClasses;
+
+namespace ChatBotMultyThread
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ChatBot.Ask("Привет");
+            string question;
+            do
+            {
+                Console.Write("-> ");
+                question = Console.ReadLine();
+                ChatBot.Ask(question);
+            } while (question != "Пока" && question != "До свидания");
+            HelpFunctions.Continue();
+        }
+    }
+}
